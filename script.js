19eeddc7611a2adf92cc9fc750a17a091c1cb373
y0hana <143098816+y0hana@users.noreply.github.com>
2024-06-11 10:58:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciado: "Pergunta 1", 
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+]
+{
+}
+    enunciado: "Pergunta 1",
+    alternativas  [
+    "Alternativa 1"
+    "Alternativa 2"
+ ]
+  
+ 
