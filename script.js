ec77a0085592b76e9767dda5eada8f51ea02eca2
y0hana <143098816+y0hana@users.noreply.github.com>
2024-06-18 11:00:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,47 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    enunciado: "Pergunta 1", 
-    alternativas: [
-        "Alternativa 1",
-        "Alternativa 2"
-    ]
-]
-{
-}
-    enunciado: "Pergunta 1",
-    alternativas  [
-    "Alternativa 1"
-    "Alternativa 2"
- ]
-  
- 
+    {
+        enunciado: "cu de prego vai visitar a amiga yohola, ajude-a com seu look"
+        alternativas: [
+            "Vestido"
+            "Calça e blusa"
+             "Saia e top"
+            
+        ]
+    },
+    {
+        enunciado: "Escolha um penteado",
+        alternativas: [
+            "Solto"
+            "Coque"
+            "Trança"
+        ]
+    }
+    {
+        enunciado: "Escolha o sapato",
+        alternativas: [
+            "Tenis"
+            "Sandália"
+            "Salto"
+        ]
+    }
+    {
+        enunciado: "Escolha a make",
+        alternativas: [
+            "Pó de banana e gloss"
+            "Riḿel, gloss e corretivo"
+            "Cara lavada"
+        ]
+    }
+    {
+        enunciado: "Para onde iremos",
+        alternativas: [
+            "Shopping"
+            "Barzinho"
+            "Restaurante"
+        ]
+    }
+]; 
+
+
