bd305ccf6db9953604fac91431a7e1d0a687f849
y0hana <143098816+y0hana@users.noreply.github.com>
2024-07-02 10:59:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,46 +6,58 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "cu de prego vai visitar a amiga yohola, ajude-a com seu look"
+        enunciado: "cu de prego vai visitar a amiga yohola, ajude-a com seu look",
         alternativas: [
-            "Vestido"
-            "Calça e blusa"
-             "Saia e top"
+            "Vestido",
+            "Calça e blusa",
             
         ]
     },
     {
         enunciado: "Escolha um penteado",
         alternativas: [
-            "Solto"
+            "Solto",
             "Coque"
-            "Trança"
         ]
-    }
+    },
     {
         enunciado: "Escolha o sapato",
         alternativas: [
-            "Tenis"
+            "Tenis",
             "Sandália"
-            "Salto"
         ]
-    }
+    },
     {
         enunciado: "Escolha a make",
         alternativas: [
-            "Pó de banana e gloss"
+            "Pó de banana e gloss",
             "Riḿel, gloss e corretivo"
-            "Cara lavada"
         ]
-    }
+    },
     {
         enunciado: "Para onde iremos",
         alternativas: [
-            "Shopping"
+            "Shopping",
             "Barzinho"
-            "Restaurante"
         ]
-    }
+    },
 ]; 
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+        perguntaAtual = perguntas[atual];
+        caixaPerguntas.textContent = perguntaAtual.enunciado;
+    }
+    
+
+mostraPergunta()
+for(const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativa.textContent = alternativa;
+    caixaAlternativas.appendChild(botaoAlternativas);
+}
+
 
 
